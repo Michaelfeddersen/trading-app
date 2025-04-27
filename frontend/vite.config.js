52496f9c8acf3052982a69e14390a14365946983
Michael Feddersen <165910787+Michaelfeddersen@users.noreply.github.com>
2025-04-28 01:14:20 +0200

    vite angepasst

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // ⬅️ HIER hinzufügen, super wichtig!
   plugins: [react()],
   server: {
     port: 5173, // oder 5174, 5175 wenn 5173 belegt ist
